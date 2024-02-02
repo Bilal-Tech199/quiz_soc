e1e35f650472c3bfb050663988502f5db7afe8c5
asad ali <asadalikfc@gmail.com>
2024-02-02 15:13:39 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+app.use(
+  cors({
+    origin: process.env.ALLOW_ORIGIN || "*",
+  })
+);
+
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: process.env.ALLOW_ORIGIN || "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`Student connected: ${socket.id}`);
+
+  socket.on("sendQuizDetails", (data) => {
+    socket.broadcast.emit("getQuizDetails", data);
+  });
+
+  socket.on("sendEndSession", (data) => {
+    socket.broadcast.emit("getEndSession", data);
+  });
+
+  socket.on("sendQuestion", (data) => {
+    socket.broadcast.emit("getQuestion", data);
+  });
+
+  socket.on("deleteQuestion", (data) => {
+    socket.broadcast.emit("deleteQuestion", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`Student disconnected: ${socket.id}`);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
