683a4108475935d98b2296f591fca7fdcf02e53e
asad ali <asadalikfc@gmail.com>
2024-02-04 18:00:55 +0500

    route added

--- server.js
+++ server.js
@@ -40,6 +40,11 @@ io.on("connection", (socket) => {
   });
 });
 
+
+app.get('/',(req,res)=>{
+  res.send('Server is running...')
+})
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
